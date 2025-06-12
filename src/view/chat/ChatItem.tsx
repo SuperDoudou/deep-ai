b16dcd32e18b73cf56013b8132359786156347fa
doudou <doudou>
2025-06-12 09:57:16 +0800

    feat: add code content

--- src/view/chat/ChatItem.tsx
+++ src/view/chat/ChatItem.tsx
@@ -1,13 +1,40 @@
 import React from 'react';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+
 
 interface ChatItemProps {
     name: string;// 用户名
     isRobot: boolean;
     avatar: string;
     message: string;
+    reasoning: string;
 }
 
-function ChatItem({ name, isRobot, avatar, message }: ChatItemProps) {
+interface MessageBlock {
+    type: string; // 消息类型，如 text, code
+    language?: string; // 消息语言，如 JavaScript, Python
+    content: string; // 消息内容
+}
+
+
+function ChatItem({ name, isRobot, avatar, message, reasoning }: ChatItemProps) {
+
+    let parseMessage = (message: string): MessageBlock[] => {
+        let tempResult: MessageBlock[] = [];
+        // 解析消息，将其拆分为不同的消息块
+        // 这里只是一个简单的示例，实际应用中需要根据具体情况进行解析
+        message.split("```").forEach((block, index) => {
+            if (index % 2 === 0) {
+                tempResult.push({ type: "text", content: block });
+            } else {
+                let [language, ...content] = block.split("\n");
+                tempResult.push({ type: "code", language: language.trim(), content: content.join("\n") });
+            }
+        });
+        return tempResult;
+    }
+    let messageBlocks = parseMessage(message)
     return (
         <div id="chat-item">
             {
@@ -25,8 +52,35 @@ function ChatItem({ name, isRobot, avatar, message }: ChatItemProps) {
                     </div>
                 )
             }
+            <div className="chat-item-reasoning-content">
+                {reasoning}
+            </div>
             <div className="chat-item-content">
-                {message}
+                {/* 遍历消息块，根据类型渲染不同的消息 */}
+                {messageBlocks.map((block, index) => {
+                    if (block.type === "text") {
+                        return <p key={index}>{block.content}</p>;
+                    }
+                    else if (block.type === "code") {
+                        return (
+                            <SyntaxHighlighter
+                                language="javascript"
+                                className="custom-scroll"
+                                style={atomDark}
+                                wrapLines={true}
+                                customStyle={{
+                                    paddingLeft: '0px',
+                                    marginLeft: '0px',
+                                    overflow: 'auto', // 
+
+                                }}  >
+                                {block.content}
+                            </SyntaxHighlighter>
+                        )
+                    }
+                })}
+
+
             </div>
         </div>
     );
--- src/view/chat/ChatItemContainer.tsx
+++ src/view/chat/ChatItemContainer.tsx
@@ -4,34 +4,53 @@ import ChatItem from './ChatItem';
 import svg from '../../resources/robot.svg'
 import App from '../App';
 import AppMessage from '../AppMessage';
-type MessageItem = {
-    name: string;
-    isRobot: boolean;
-    avatar: string;
-    message: string;
+import LLMService from './LLMService';
+
+class MessageItem {
+    public name: string = "";
+    public isRobot: boolean = true;
+    public avatar: string = "";
+    public message: string = "";
+    public reasoningContent: string = "";
+    public setMessage = (message: string) => {
+        this.message = message;
+    };
+    public setReasoningContent = (reasoningContent: string) => {
+        this.reasoningContent = reasoningContent;
+    };
+    constructor(name: string, isRobot: boolean, avatar: string, message: string, reasoningContent: string) {
+        this.name = name;
+        this.isRobot = isRobot;
+        this.avatar = avatar;
+        this.message = message;
+        this.reasoningContent = reasoningContent;
+    }
 };
 
 function ChatContainer() {
 
     const [messagesList, setMessagesList] = useState<MessageItem[]>([])
 
+    const [count, setCount] = useState(0);
+
     useEffect(() => {
+        LLMService.init()
         let randMessage = mockMessages[Math.floor(Math.random() * mockMessages.length)]
-        pushMessagesList({
-            name: randMessage.name,
-            avatar: randMessage.avatar,
-            isRobot: true,
-            message: '你好，这是第一条消息'
-        })
+        let messageItem = new MessageItem()
+        messageItem.name = randMessage.name
+        messageItem.isRobot = randMessage.isRobot
+        messageItem.avatar = randMessage.avatar
+        messageItem.message = '你好，这是第一条消息'
+        pushMessagesList(messageItem)
     }, [])
+
+
 
     let pushMessagesList = (message: MessageItem) => {
         // debugger
         console.log(`push message ${message.message}`)
         setMessagesList(messagesList => {
-            let temp = [...messagesList]
-            temp.push(message)
-            return temp
+            return messagesList.concat(message)
         })
     };
 
@@ -41,41 +60,58 @@ function ChatContainer() {
 
     const onInputMessage = (message: string) => {
         console.log(`get input message ${message}`)
-        pushMessagesList({
-            name: mockUser.name,
-            isRobot: false,
-            avatar: mockUser.avatar,
-            message: message
-        })
+        let messageItem = new MessageItem(mockUser.name, false, mockUser.avatar, message, "")
+        pushMessagesList(messageItem)
 
+        let answerMessageItem = new MessageItem(mockUser.name, true, mockUser.avatar, "", "")
+        pushMessagesList(answerMessageItem)
+        LLMService.sendText(message,
+            (m) => {
+                answerMessageItem.setReasoningContent(m);
+                setCount(count => count + 1)
+
+                window.scrollTo({
+                    top: document.body.scrollHeight,
+                    behavior: 'smooth'
+                });
+            },
+            (m) => {
+                answerMessageItem.setMessage(m);
+                setCount(count => count + 1)
+
+                window.scrollTo({
+                    top: document.body.scrollHeight,
+                    behavior: 'smooth'
+                });
+            })
         AppMessage.sendMessageToParent()
-        setTimeout(() => {
-            let randMessage = mockMessages[Math.floor(Math.random() * mockMessages.length)]
-            pushMessagesList(randMessage)
 
-        }, 2000);
     }
 
     const mockUser = { name: '企鵝豆豆', avatar: svgToDataURL(svg) }
 
 
     const mockMessages: MessageItem[] = [
-        { name: '用户1', isRobot: true, avatar: svgToDataURL(svg), message: '你好，这是第一条消息' },
-        { name: '用户2', isRobot: true, avatar: svgToDataURL(svg), message: '今天的会议安排如何？' },
-        { name: '用户3', isRobot: true, avatar: svgToDataURL(svg), message: '请查看最新文档' },
-        { name: '用户4', isRobot: true, avatar: svgToDataURL(svg), message: '项目进度更新' },
+        new MessageItem('用户1', true, svgToDataURL(svg), '你好，这是第一条消息', '这是第一条消息的思考过程'),
+        new MessageItem('用户2', true, svgToDataURL(svg), '今天的会议安排如何？', '这是第一条消息的思考过程222'),
     ]
 
 
     return (
         <div id="chat_container">
             {messagesList.map((item, index) => (
-                <ChatItem key={index} name={item.name} isRobot={item.isRobot} avatar={item.avatar} message={item.message}>
+                <ChatItem key={index}
+                    name={item.name}
+                    isRobot={item.isRobot}
+                    avatar={item.avatar}
+                    message={item.message}
+                    reasoning={item.reasoningContent}>
                 </ChatItem>
             ))}
 
 
             <InputArea onSendMessage={onInputMessage}></InputArea>
+
         </div>
     );
 }
